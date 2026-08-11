a55260620d4f01fa8fd5b43bdadf340f5295218a
Robbie_Wagner <robbie.zoborowi@gmail.com>
2026-08-11 14:14:55 -0500

    work on mod setup

---
+++ MyFirstBigWalkMod/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace MyFirstBigWalkMod;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    internal static new ManualLogSource Log;
+
+    public override void Load()
+    {
+        var Harmony = new Harmony("com.pompousrhombus.myfirstbigwalkmod");
+        Harmony.PatchAll();
+        
+        // Plugin startup logic
+        Log = base.Log;
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
